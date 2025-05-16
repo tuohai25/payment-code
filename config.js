a2d8a881947bf377e8cc50227c75d9f808730e50
tuohai25 <823240516@qq.com>
2025-05-17 01:41:04 +0800

    changePicture

--- config.js
+++ config.js
@@ -3,9 +3,9 @@
 const basic = { // 基础设置
     favicon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👻</text></svg>', // 页面图标
     title: '(～￣▽￣)～', // 页面标题
-    avatar: '823240516', // 头像 URL，也可用 QQ 号 (e.g. 10000) 或 Github 用户名 (e.g. @alex3236)
+    avatar: '../img/profile_picture.jpg', // 头像 URL，也可用 QQ 号 (e.g. 10000) 或 Github 用户名 (e.g. @alex3236)
     name: '', // 头像下的昵称
-    sign: '<del>微信转账300块,帮我回香港</del>', // 个性签名或提示文字，可使用 HTML 格式
+    sign: '<del>我很可爱，请给我钱</del>', // 个性签名或提示文字，可使用 HTML 格式
     user_page: '', // 点击头像或名字时跳转的链接，留空或删除则不跳转
     footer: '', // 页脚文字，可使用 HTML 格式
     uri_redirect: true // 若收款码 URL 是网址，是否直接跳转而不显示二维码
